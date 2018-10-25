d54ff24816494ee10613d1a83b32a6bac7c7b50b
Daniel Monteiro <daniell.monteirosousa@gmail.com>
2018-10-25 01:04:21 -0300

    andamento

--- trabalho_lfa_django_app/views.py
+++ trabalho_lfa_django_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
 # Create your views here.
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def verify_mt(request):
+    try:
+        
+        return
+    except Exception, e:
+        return
